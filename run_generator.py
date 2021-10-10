b69623a7798fc56b27c8d36eca7e59592265757f
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-10-10 21:19:29 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,9 @@
-import os as alpha
-alpha.system("nvidia-smi")
+FROM ubuntu:20.04 as npm_build
+
+## Install docker
+RUN apt-get update && \
+    DEBIAN_FRONTEND="noninteractive" apt-get install -y apt-transport-https ca-certificates curl software-properties-common build-essential && \
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && \
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && \
+    apt-get update && \
+    apt-get install -y docker-ce
